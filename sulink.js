ce687cfc7843f9ceb21deea12e5330d7f8a4fe7f
Pikas-cyber <StivesFargnoli@gmail.com>
2024-07-23 06:49:13 -0700

    Update sulink.js

--- sulink.js
+++ sulink.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 // 网站名字
 var originalTitle = "速联云";
 // 机场的地址url
-var wangzhangurl = "https://sulink.xyz";
+var wangzhangurl = "https://sulink.icu";
 // 登录页面背景图
 var loginBackgroundImageUrl = 'https://cdn.jsdelivr.net/gh/Pikas-cyber/vpn@main/sulink/400x165.png';
 // 登录后的背景图
